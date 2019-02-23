d22b1867916e01a1180fc99278bc47669f99ebdf
MRCR0TD1LU4R <47303389+MRCR0TD1LU4R@users.noreply.github.com>
2019-02-23 21:30:03 +0700

    T4MP4N.sh
    Cr0t_d1lu4r

---
+++ T4MP4N.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+figlet TES KETAMPANAN.PHP |lolcat
+
+echo "{×}==================================={×}"|lolcat
+echo "Creator: MR.CR0T_D1LU4R"|lolcat
+echo "-----------------------------------------"|lolcat
+echo "Team   : 1.SOLO PLAYER"|lolcat
+echo "         2.CWD+ROBH"|lolcat
+echo "-----------------------------------------"|lolcat
+echo "Thanks : > CR0T AND TEAM"|lolcat
+echo "         > CR0T_D1LU4R GANS"|lolcat
+echo "         > BOKEP=VANESSA ANGEL=INDO"|lolcat
+echo "{×}==================================={×}"|lolcat
+echo ""   
+echo "Oke mek gausah banyak bacod kita buktikan ketampanan kalian"|lolcat
+echo "___________________________________________________"|lolcat
+echo ""
+echo "1.> TES KETAMPANAN SENDIRI    "$red
+echo ""
+echo "2.> TES KETAMPANAN TEMAN      "$green
+echo "___________________________________________________"|lolcat
+
+echo ""
+echo "\033[34;1m  
+99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m CEPET PILIH BURIQ!\033[32;1m"
+read -p " PILIHAN=> " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet TES KETAMPANAN| lolcat
+echo "\033[34;1mTUNGGU EA JEMBOED"
+echo ""
+sleep 1
+
+echo "TES KETAMPANAN SIAPA" 
+echo ""
+echo ""
+echo "TES MUKA LU?"
+read -p "MASUKAN NAMA ASLI LU=>" NAMA
+echo "tgl/bln/thn"  
+echo ""
+echo ""
+echo "ALAMAT PALSU JUGA GPP:" $pulsa|lolcat
+echo "KELAMIN :" $nomor |lolcat
+echo ""
+echo ""
+sleep 2
+clear
+figlet LOADING|lolcat
+sleep 15
+echo ""
+echo "MUKA TERDETECE"|lolcat
+echo ""
+sleep 2
+echo "JELEK JELEK JELEK JELEK JELEK JELEK JELEK"|lolcat
+echo "BURIQ BURIQ  BURIQ BURIQ BURIQ, OM CR0T_D1LU4R TAMPAN"|lolcat
+echo ""
+sleep 2
+echo "Jangan marah ya tod kalo emang BURIQ YAH BURIQ AJA"|lolcat
+echo "PESAN GUA"|lolcat
+sleep 2
+echo "BANYAKIN COLI BIAR TAMPAN"|lolcat
+echo "By./MR.CR0T_D1LU4R GANS"|lolcat
+sleep 2
+figlet B . Y . E|lolcat
+fi
